678ed0ac88869ffbf3153c9892c8287d7b1fd310
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-13 23:19:05 +0530

    Jul 13  23:19:05

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -7,11 +7,11 @@
 
 def fun_ismultiple(m, n):
 
-	if (m % n == 0) or n==0 and f==0:
+	if (m % n == 0) or n==0 and m==0:
 		return True
-	elif f == 0:
+	elif n == 0:
 		return False
-	if n % f == 0 :
+	if m == n :
 		return True
 	else:
 		return False # replace with your solution
