3c5788f16e7e0e13dd259dc12f495af31598d7b0
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-13 22:11:56 +0530

    Jul 13  22:11:56

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -7,11 +7,12 @@
 
 def fun_isfactor(f, n):
 	
-	if n == 0 and f == 0:
+	if (n == 0 and f == 0) :
 		return True
+	
+	elif f == 0:
+		return False
 	if n % f == 0 :
 		return True
-	if f == 0:
-		return False
 	else :	
 		return False
