23c18a22f79eb1ad7458f54d2725df3fca49101c
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-14 18:03:39 +0530

    Jul 14  18:03:39

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -5,5 +5,4 @@
 # the triangleArea function you just wrote above.
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+	return (x1*(y1-y2) + x2 * (y2-y3) + x3 * (y3-y1))/2
