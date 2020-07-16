5e3c9f28cc45dfe3883cb717c2ded12a3a260efd
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-16 22:53:17 +0530

    Jul 16  22:53:17

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -10,6 +10,27 @@
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
 
-
+def ishappynumber(n):
+	def sumofsquares(num1):
+		add = 0
+		while(num1 > 0):
+			r = num1 % 10
+			add = add + (r**2)
+			num1 = num1 // 10
+		return add
+	l =[]
+	while sumofsquares(n) not in l:
+		res = sumofsquares(n)
+		if res == 1:
+			return True
+		else:
+			l.append(res)
+			n = res
+	return False
 def fun_nth_happy_number(n):
-	return 0
+	li = []
+	for i in range(60):
+		if ishappynumber(i):
+			li.append(i)
+	return li[n]
+
