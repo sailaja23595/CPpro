6e1f88ac543d17448f81afa37c4ec92fdbb081c4
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-16 23:17:05 +0530

    Jul 16  23:17:05

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,33 @@
 
 
 
+def ishappynumber(n):
+	def sumofsquares(num1):
+		add = 0
+		while(num1 > 0):
+			r = num1 % 10
+			add = add + (r**2)
+			num1 = num1 // 10
+		return add
+	l = []
+	while sumofsquares(n) not in l:
+		res = sumofsquares(n)
+		if res == 1:
+			return True
+		else:
+			l.append(res)
+			n = res
+	return False
+def isPrime(n):
+	for i in range(2,n):
+		if(n%i ==0):
+			return False
+		else:
+			return True
+
 def fun_nth_happy_prime(n):
-	return 0
+	li = []
+	for i in range(60):
+		if ishappynumber(i) and isPrime(i):
+			li.append(i)
+	return li[n]
