b4870edf71fa5baabbe3babc1f37e209dde141df
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-21 19:36:08 +0530

    manual push of recursion power of 3

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -2,11 +2,19 @@
 # Write the function powersOf3ToN(n) that takes a possibly-negative float or int n, and returns a list of the 
 # positive powers of 3 up to and including n. As an example, powersOf3ToN(10.5) returns [1, 3, 9]. If no such powers 
 # of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
-
+import math
 def recursion_powersof3ton(n):
-	if n == 0:
+	n = int(n)
+	if n <= 0:
 		return None
 	if n == 1:
 		return [1]
-	if n >= 3:
-		return int(3 * power(3,n-1))
+	else:
+		num = int(math.pow(3,19))
+		# print(num)
+		if num % n == 0:
+			return sorted([n]+ recursion_powersof3ton(n-1))
+		else:
+			return recursion_powersof3ton(n-1)
+
+# recursion_powersof3ton(9)
