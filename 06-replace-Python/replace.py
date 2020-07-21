555af7519e03e655ab30dd0c032b51377bce4b89
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-21 22:36:40 +0530

    manual push of replace

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -5,10 +5,26 @@
 
 
 def fun_replace(s1, s2, s3):
-	for i in range(len(s1)):
-		while(i < len(s3)):
-			if(s1[i] == s2[i]):
-				s1[i] = s3[i]
-			i = i + 1
-	return s1
+	# for i in range(len(s1)):
+	# 	while(i < len(s3)):
+	# 		if(s1[i] == s2[i]):
+	# 			s1[i] = s3[i]
+	# 		i = i + 1
+	# return s1
+	c = s1.count(s2)
+	if c==0:
+		return s1
+	elif c==1:
+		st = ''
+		start = s1.find(s2)
+		end = start + len(s2)
+		st = s1[0:start] + s3 + s1[end:]
+		return st
+	else:
+		st = s1
+		for i in range(c):
+			start = st.find(s2)
+			end = start + len(s2)
+			st = st[0:start] + s3 + st[end:]
+		return st
 
