ebeded20031fafc956af9d586fe211f9e431a577
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-22 16:13:28 +0530

    Jul 22  16:13:28

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -5,5 +5,15 @@
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+	if not n:
+		return 0
+	longest =1
+	current = 1
+	for i in range(1, len(n)):
+		if n[i] != n[i-1]:
+			if n[i] == n[i - 1] + 1:
+				current += 1
+			else:
+				longest = max(longest,current)
+				current = 1
+	return max(longest)
