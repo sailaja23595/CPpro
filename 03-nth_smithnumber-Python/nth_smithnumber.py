7f52b3bf72523a3e2185b85104cd11e84f01ca64
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-23 15:53:23 +0530

    Jul 23  15:53:23

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,5 +8,50 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+def addition(n):
+    add = 0
+    while n > 0:
+        r = n % 10
+        add = add + r
+        n = n//10
+    return add
+
+def addfactors(n):
+    li = []
+    while n > 0 and n % 2 == 0:
+        li.append((int(2)))
+        n = n / 2
+    for i in range(3,int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            li.append(int(i))
+            n = n / i
+    if n > 2:
+        li.append(int(n))
+    add = 0
+    for i in li :
+        if len(str(i)) > 1 and i is not n:
+            add  = add + addition(i)
+        elif len(str(i)) == 1:
+            add  = add + i
+    return add
+
+def isprime(n):
+    if  n > 1:
+        for i in range(2,n):
+            if(n % i != 0):
+                return True
+        return False
+    return False
+
+def issmith(n):
+    if addition != addfactors(n):
+        return False
+    else:
+        return True
+
 def fun_nth_smithnumber(n):
-    return 1
+    l = []
+    for i in range(1,1000):
+        if issmith(i) and not isprime(i):
+            l.append(i)
+    return l[n]
