39b47170b88ffa8842bda1160721436dc6e19b15
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-24 14:11:45 +0530

    nth tidy number manual push

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,20 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def tidynumber(n):
+    pre = 10
+    while(n):
+        r = n % 10
+        n = n // 10
+        if r > pre:
+            return False
+        pre = r
+    return True
+
 def fun_nth_tidynumber(n):
-    return 0
+    li = []
+    for i in range(3000):
+        if tidynumber(i):
+            li.append(i)
+    return li[n+1]
+ 
