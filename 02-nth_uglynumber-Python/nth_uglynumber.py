2a8b8890c6deefa5447644f17fa3ea24fdda9e2b
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-24 14:33:34 +0530

    manual push  of nth ugly number

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -2,19 +2,24 @@
 # Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 
 # 9, 10, 12, 15 shows the few ugly numbers. By convention, nthUglyNumber(0) will give 1.
 
-def primefact(n):
-    while n%2==0:
-        n = n/2
-    return n
+# def primefact(n):
+#     while n%2==0:
+#         n = n/2
+#     return n
 
 def uglynumber(n):
-    n = primefact(n,2)
-    n = primefact(n,3)
-    n = primefact(n,5)
     if n==1:
         return 1
-    else:
+    if n<=0:
         return 0
+
+    if(n%2==0):
+        return uglynumber(n//2)
+    if(n%3==0):
+        return uglynumber(n//3)
+    if(n%5==0):
+        return uglynumber(n//5)
+    return 0
 
 def fun_nth_uglynumber(n):
     li = []
