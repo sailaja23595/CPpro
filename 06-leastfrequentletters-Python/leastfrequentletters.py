583336be312898a3e31a807c7f760f25e867c61d
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-07-25 18:01:05 +0530

    Jul 25  18:01:05

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -7,12 +7,37 @@
 # yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
+# def splitstr(str):
+# 	alp = ""
+# 	number = ""
+# 	spe = ""
+# 	for i in range(len(str)):
+# 		if(str[i].isdigit()):
+# 			number = number + str[i]
+# 		elif((str[i] >= 'A' and str[i]  <= 'Z')or (str[i] >= 'a' and str[i] <= 'z' ))
 def leastfrequentletters(s):
-	string = ""
-	if s.isalpha():
-		for i in s:
-			if string in i:
-				string[i] = string[i] + 1
-			return string 
-	return ''
+	if len(s) == 0:
+		return ''
+	s = s.lower()
+	alp = string.ascii_lowercase
+	minc = 1500
+	res = ''
+	i = 0
+	while i < 26:
+		c = s.count(alp[i])
+		if minc > c > 0 or minc == c:
+			minc = c
+			res = alp[i]
+		i = i+1
+	if res == '':
+		return res
+	
+	
+	# string = ""
+	# if s.isalpha():
+	# 	for i in s:
+	# 		if string in i:
+	# 			string[i] = string[i] + 1
+	# 		return string 
+	# return ''
 		
